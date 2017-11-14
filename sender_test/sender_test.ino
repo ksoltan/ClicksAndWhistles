a16ec6a606772b4b6be5fdc399f38b149ab012e3
SeanCCarter <seancarter26@gmail.com>
2017-11-14 14:58:38 -0500

    Adding sending over wire.

---
+++ sender_test/sender_test.ino
@@ -0,0 +1,23 @@
+// Include the required Wire library for I2C
+#include <Wire.h>
+int x = 0;
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+  Serial.begin(9600); //Enable the serial comunication
+}
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  Serial.println("Writing: " + String(x));
+  x++; // Increment x
+  
+  if (x > 5) x = 0; // `reset x once it gets 6
+  delay(1000);
+}
+
+void commandMotor(char[2] motor, int motorPos){
+  Wire.write(motor + motorPos + ";");
+}
+
